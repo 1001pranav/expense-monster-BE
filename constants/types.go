a91a273de0aaf3228a29f987afc952dda17ede4e
npranavr <npranavr@gmail.com>
2024-02-21 16:59:51 +0530

    Feat: Added Models and Enum for all tables

--- constants/types.go
+++ constants/types.go
@@ -1,55 +1,111 @@
 package constants
 
+import (
+	"time"
+)
+
 type LoginAPIData struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 type StatusType string
+type TransactionType string
+type InvestmentType string
+type FrequencyType string
 
 const (
-	ACTIVE   StatusType = "Active"
-	BLOCKED  StatusType = "Blocked"
-	INACTIVE StatusType = "Inactive"
-	DELETED  StatusType = "Deleted"
+	STATUS_ACTIVE   StatusType = "Active"
+	STATUS_BLOCKED  StatusType = "Blocked"
+	STATUS_INACTIVE StatusType = "Inactive"
+	STATUS_DELETED  StatusType = "Deleted"
+	STATUS_PAUSED   StatusType = "Paused"
+	STATUS_PENDING  StatusType = "Pending"
 )
 
-type ModelUsers struct {
-	UserID    uint   `json:"user_id"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	CreatedOn uint64 `json:"created_on"`
-	UpdatedAt uint64 `json:"updated_at"`
-	Status    string `json:"status"`
-}
+const (
+	TRANSACTION_INCOME     TransactionType = "Income"
+	TRANSACTION_EXPENSE    TransactionType = "Expense"
+	TRANSACTION_INVESTMENT TransactionType = "Expense"
+)
 
+const (
+	INVESTMENT_RD     InvestmentType = "RD"
+	INVESTMENT_FD     InvestmentType = "FD"
+	INVESTMENT_STOCKS InvestmentType = "Stocks"
+	INVESTMENT_MF     InvestmentType = "MF"
+	INVESTMENT_GOLD   InvestmentType = "Gold"
+	INVESTMENT_OTHERS InvestmentType = "Others"
+)
+
+const (
+	FREQUENCY_DAILY         FrequencyType = "Daily"
+	FREQUENCY_WEEKLY        FrequencyType = "Weekly"
+	FREQUENCY_MONTHLY       FrequencyType = "Monthly"
+	FREQUENCY_YEARLY        FrequencyType = "Annually"
+	FREQUENCY_QUARTERLY     FrequencyType = "Quarterly"
+	FREQUENCY_SEMI_ANNUALLY FrequencyType = "Semi-Annually"
+)
+
+type ModelUser struct {
+	UserID    int        `json:"user_id" gorm:"primary_key;auto_increment"`
+	Email     string     `json:"email" gorm:"type:varchar"`
+	Password  string     `json:"password" gorm:"type:varchar"`
+	CreatedOn time.Time  `json:"created_on" gorm:"type:timestamp; default:current_timestamp"`
+	UpdatedAt time.Time  `json:"updated_at" gorm:"type:timestamp; default:current_timestamp"`
+	Status    StatusType `gorm:"type:varchar" json:"status"`
+}
 type ModelCards struct {
-	CardId   uint   `json:"card_id"`
-	CardName string `json:"card_name"`
-	UserID   uint   `json:"user_id"`
-	Limit    uint   `json:"limit"`
-	LastDay  uint   `json:"last_day"`
-	Expiry   uint   `json:"expiry"`
-	Status   string `json:"status"`
+	CardID   int         `gorm:"primary_key;auto_increment" json:"card_id"`
+	CardName string      `gorm:"type:varchar" json:"card_name"`
+	UserID   []ModelUser `json:"user_id" gorm:"foreignkey:UserID"`
+	Limit    float64     `json:"limit"`
+	LastDay  time.Time   `gorm:"type:timestamp" json:"last_day"`
+	Expiry   time.Time   `gorm:"type:timestamp" json:"expiry"`
+	Status   StatusType  `gorm:"type:varchar" json:"status"`
+}
+type ModelUserCategory struct {
+	UCID         int             `gorm:"primary_key;auto_increment" json:"uc_id"`
+	UserID       []ModelUser     `gorm:"foreignkey:UserID" json:"user"`
+	CategoryType TransactionType `json:"category_type" gorm:"type:varchar"`
+	CategoryName string          `gorm:"type:varchar" json:"category_name"`
+	CreatedOn    time.Time       `gorm:"type:timestamp; default:current_timestamp" json:"created_on"`
 }
 
-type ModelUserCategory struct {
-	UCID         uint   `json:"uc_id"`
-	UserID       uint   `json:"user_id"`
-	CategoryType string `json:"category_type"`
-	CategoryName string `json:"category_name"`
-	CreatedOn    int    `json:"created_on"`
+type ModelBill struct {
+	BillID         int           `gorm:"primary_key;auto_increment" json:"bill_id"`
+	Frequency      FrequencyType `json:"frequency" gorm:"type:varchar"`
+	OtherFrequency *time.Time    `gorm:"type:timestamp" json:"other_frequency"`
+	ReminderAlert  bool          `json:"reminder_alert" gorm:"default:false"`
+	AutoPay        bool          `json:"auto_pay" gorm:"default:false"`
+	Description    *string       `gorm:"type:varchar" json:"description"`
+	Status         StatusType    `gorm:"type:varchar" json:"status"`
+}
+
+type ModelInvestment struct {
+	InvestmentID        int            `gorm:"primary_key;auto_increment" json:"investment_id"`
+	Type                InvestmentType `json:"type" gorm:"type:varchar"`
+	OtherType           *string        `gorm:"type:varchar" json:"other_type"`
+	Description         *string        `gorm:"type:varchar" json:"description"`
+	EndsOn              *time.Time     `gorm:"type:timestamp" json:"ends_on"`
+	Returns             *float64       `gorm:"default:0" json:"returns"`
+	Frequency           FrequencyType  `json:"frequency" gorm:"type:varchar"`
+	OtherFrequency      *time.Time     `gorm:"type:timestamp" json:"other_frequency"`
+	AutoPaid            bool           `json:"auto_paid" gorm:"default:false"`
+	SkipNextInstallment bool           `json:"skip_next_installment" gorm:"default:false"`
+	CreatedOn           time.Time      `gorm:"type:timestamp; default:current_timestamp" json:"created_on"`
+	Status              StatusType     `gorm:"type:varchar" json:"status"`
 }
 
 type ModelTransaction struct {
-	TransactionID uint    `json:"transaction_id"`
-	Name          string  `json:"name"`
-	Type          string  `json:"type"`
-	CategoryID    uint    `json:"category_id"`
-	UserID        uint    `json:"user_id"`
-	Amount        float64 `json:"amount"`
-	CreditCardID  uint    `json:"credit_card_card_id"`
-	BillID        uint    `json:"bill_id"`
-	InvestmentID  uint    `json:"investment_id"`
-	TransactionOn int     `json:"transaction_on"`
+	TransactionID int                 `gorm:"primary_key;auto_increment" json:"transaction_id"`
+	Name          string              `gorm:"type:varchar" json:"name"`
+	Type          TransactionType     `gorm:"type:varchar" json:"type"`
+	CategoryID    []ModelUserCategory `gorm:"foreignkey:CategoryID" json:"category_id"`
+	UserID        []ModelUser         `gorm:"foreignkey:UserID" json:"user_id"`
+	Amount        float64             `json:"amount"`
+	CreditCardID  *[]ModelCards       `json:"credit_card_id" gorm:"foreignkey: CardID"`
+	BillID        *[]ModelBill        `json:"bill_id" gorm:"foreignkey: BillID"`
+	InvestmentID  *[]ModelInvestment  `json:"investment_id" gorm:"foreignkey: InvestmentID"`
+	TransactionOn time.Time           `gorm:"type:timestamp" json:"transaction_on"`
 }
