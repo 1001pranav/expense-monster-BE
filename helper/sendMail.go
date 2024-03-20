f67fe4c8009a260a9777b13f6e943c3eca891b7b
npranavr <npranavr@gmail.com>
2024-03-20 11:47:33 +0530

    Feat: Added Mail feature and commented mail sending without proper credentials

---
+++ helper/sendMail.go
@@ -0,0 +1,19 @@
+package helper
+
+// import (
+// 	"fmt"
+// 	"net/smtp"
+// )
+
+func SendMailSMTP(toMailAddress string, message []byte) error {
+	// senderEmailAddress := ""
+	// smtpUserName := ""
+	// smtpPassword := ""
+	// smtpHost := ""
+	// smtpPort := 124
+	// auth := smtp.PlainAuth("", smtpUserName, smtpPassword, smtpHost)
+	// to := []string{toMailAddress}
+	// serverAddr := fmt.Sprintf("%s:%d", smtpHost, smtpPort)
+
+	return nil //smtp.SendMail(serverAddr, auth, senderEmailAddress, to, message)
+}
